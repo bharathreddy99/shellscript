ce16d026a9645554f49d1f74fc6b298a7c809183
Bharath <kbharathreddy99@gmail.com>
2023-08-21 17:17:14 +0530

    first

--- Read.sh
+++ Read.sh
@@ -4,4 +4,8 @@ echo "welcome to read shellscrpting"
 
 read USERNAME # os will wait for imput to take from user
 
-echo " Hi,$USERNAME you are logged in" 
+echo " Hi,$USERNAME you are logged in"
+
+read -s ADMINNAME #it will wait for input to take from user
+
+echo "you successfully logged in as $ADMIN"
