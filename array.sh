a5de1b4d974fb3ab127f46aab51a6eddfe6caa63
Bharath <kbharathreddy99@gmail.com>
2023-08-21 17:29:51 +0530

    first

--- array.sh
+++ array.sh
@@ -2,6 +2,6 @@
 
 #Multiple values is holding by variable
 
-persons =("ravi" "kumar" "raghu" )
-
+persons=("ravi" "kumar" "raghu" )
+#inside array we refer values with index, it starts from 0
 echo "first person ${persons(0)}"
