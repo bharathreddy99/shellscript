7950f618d7cab09ef61fcd1aa7eb9a6326e536e7
Bharath <kbharathreddy99@gmail.com>
2023-08-21 18:16:20 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -2,10 +2,10 @@
 
 
 Read Number
-if[$Number -gt 10]
 
- then 
-   echo " The number is greater than 10"
+if [$Number -gt 10]
+then 
+   echo " The number is greater than 10"    
 else
    echo " The number is less than 10"
 fi
