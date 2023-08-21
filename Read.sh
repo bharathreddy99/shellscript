021bd58124336bc20fd7b4d066467c152997b687
Bharath <kbharathreddy99@gmail.com>
2023-08-21 17:29:07 +0530

    first

--- Read.sh
+++ Read.sh
@@ -4,6 +4,7 @@ echo "welcome to read shellscrpting"
 
 read USERNAME # os will wait for imput to take from user
 
+#Only for validation and practice purpose
 echo " Hi,$USERNAME you are logged in"
 
 read -s ADMINNAME #it will wait for input to take from user
---
+++ array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Multiple values is holding by variable
+
+persons =("ravi" "kumar" "raghu" )
+
+echo "first person ${persons(0)}"
