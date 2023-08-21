cfdcba9e3e1ce1279981fe8ed62b5c843e97224b
Bharath <kbharathreddy99@gmail.com>
2023-08-21 17:08:17 +0530

    first

--- adding.sh
+++ adding.sh
@@ -6,6 +6,6 @@ Number1=$1 #first arugment from command line
 Number2=$2 #second arugment from command line
 
 #Sum of two numbers
-SUM=$(Number1+Number2)
+SUM=$((Number1+Number2))
 
 echo "Sum of two number is $SUM"
