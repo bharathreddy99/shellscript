d52d3e09ab76b451f20323aaaa087f664ded3560
Bharath <kbharathreddy99@gmail.com>
2023-08-21 17:03:01 +0530

    first

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo " Hi welcome to variables shell script"
+
+Person1=$1 #first arugment from command line
+Person2=$2 #second arugment from command line
+
+echo "$Person1:Hi $Person2,GM"
+echo "$Person2:Hi $Person1,GoodMrng"
