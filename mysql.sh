582740182f16140964dbde158c77bcb4aed9c742
Bharath <kbharathreddy99@gmail.com>
2023-08-21 20:05:31 +0530

    first

--- mysql.sh
+++ mysql.sh
@@ -7,6 +7,14 @@ then
    echo "Please login to root user"
    exit 1
 #else
-   #echo "you are root user"
+   echo "you are root user"
 fi
    yum install mysql -y
+
+if [ $? -ne 0]
+then 
+   echo "Installation is unsuccessful"
+   exit 1
+else  
+   echo "Installation is successful"
+fi
