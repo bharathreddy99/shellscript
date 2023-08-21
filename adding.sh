f99bc6d8f0bd0fc4971349d48ec8068d6a9cdf95
Bharath <kbharathreddy99@gmail.com>
2023-08-21 17:07:32 +0530

    first

---
+++ adding.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo " Hi welcome to Adding shell script"
+
+Number1=$1 #first arugment from command line
+Number2=$2 #second arugment from command line
+
+#Sum of two numbers
+SUM=$(Number1+Number2)
+
+echo "Sum of two number is $SUM"
