ea15fc3670fef9dee41be84b99f8635e1380cc34
Bharath <kbharathreddy99@gmail.com>
2023-08-21 16:55:32 +0530

    first

--- Helloworld.sh
+++ Helloworld.sh
@@ -5,5 +5,5 @@ echo " Hi welcome to first shell script"
 Person1=ramesh
 Person2=suresh
 
-echo"$Person1:Hi $Person2,GM"
-echo"$Person2:Hi $Person1,GoodMrng"
+echo "$Person1:Hi $Person2,GM"
+echo "$Person2:Hi $Person1,GoodMrng"
